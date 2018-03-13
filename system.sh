6306b6c3eae659b325997c897359bac180aba008
Adam McClenaghan <adam@mcclenaghan.co.uk>
2018-03-13 18:50:17 +0000

    Add a few more installs

--- system.sh
+++ system.sh
@@ -6,8 +6,8 @@ echo "Initialising System Setup..."
 sudo apt-get -y update
 
 # Commonly used tools
-sudo apt-get install -y tcpdump dnsutils tmux tor maven 
+sudo apt-get install -y tcpdump dnsutils tmux tor maven tree curl python3-pip 
 
 #Add GNOME
 sudo add-apt-repository ppa:gnome3-team/gnome3
-sudo apt-get update && sudo apt-get install gnome-shell ubuntu-gnome-desktop
+sudo apt-get update && sudo apt-get install -y gnome-shell ubuntu-gnome-desktop
