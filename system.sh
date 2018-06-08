9dbe2f143d938308492619f6903b6533bd2a3195
adammcclenaghan <adam@mcclenaghan.co.uk>
2018-06-09 00:39:19 +0100

    Adding some more tools

--- system.sh
+++ system.sh
@@ -2,8 +2,8 @@
 
 echo "Initialising System Setup..."
 
-#Update repos
+# Update repos
 sudo apt-get -y update
 
 # Commonly used tools
-sudo apt-get install -y tcpdump dnsutils tmux tor maven tree curl python3-pip 
+sudo apt-get install -y tcpdump dnsutils tmux tor maven tree curl python3-pip wireshark emacs 
