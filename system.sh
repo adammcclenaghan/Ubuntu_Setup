49962b5603797e99fd1abab57520885e57f4eff3
adammcclenaghan <adam@mcclenaghan.co.uk>
2018-06-09 00:30:06 +0100

    Gnome standard on 18.04 now

--- system.sh
+++ system.sh
@@ -7,7 +7,3 @@ sudo apt-get -y update
 
 # Commonly used tools
 sudo apt-get install -y tcpdump dnsutils tmux tor maven tree curl python3-pip 
-
-#Add GNOME
-sudo add-apt-repository ppa:gnome3-team/gnome3
-sudo apt-get update && sudo apt-get install -y gnome-shell ubuntu-gnome-desktop
