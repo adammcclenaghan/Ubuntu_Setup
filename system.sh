4dfe76149dfefa9ab7d04fed2ccee171108789a4
adammcclenaghan <adam@mcclenaghan.co.uk>
2018-06-23 17:35:28 +0100

    Add support for changing wallpaper

--- system.sh
+++ system.sh
@@ -6,4 +6,18 @@ echo "Initialising System Setup..."
 sudo apt-get -y update
 
 # Commonly used tools
-sudo apt-get install -y tcpdump dnsutils tmux tor maven tree curl python3-pip wireshark emacs 
+sudo apt-get install -y tcpdump dnsutils tmux tor maven tree curl python3-pip wireshark emacs
+
+#Setup Wallpaper - Gnome only
+while true; do
+	read -p "Do you wish to change your wallpaper to the one included with this repo? (Only supported on gnome via gsettings)" yn
+	case $yn in
+		[Yy]* ) gsettings set org.gnome.desktop.background picture-uri file:///$current_repo/wallpaper.jpeg ; break;;
+		[Nn]* )  break;;
+		* ) echo "Please enter yes or no";;
+	esac
+done
+
+read -p 'y/n: ' changeWallpaper
+
+
